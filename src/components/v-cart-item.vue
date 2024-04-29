94428b334c78b913f34cfb367d202dbda056b829
Trifonix <trifonix@rocketmail.com>
2024-04-29 16:29:55 +0300

    fix v-car-item errors

--- src/components/v-cart-item.vue
+++ src/components/v-cart-item.vue
@@ -1,45 +1,44 @@
 <template>
-    <div class="v-cart-item">
-      <vCatalog />
-      <vCart />
-    </div>
-  </template>
-  
-  <script>
-  import vCatalog from './v-catalog.vue'
-  import vCart from './v-cart.vue'
-  
-  export default {
-    name: 'v-cart-item',
-    components: {
-      vCatalog,
-      vCart
-    },
-    props: {},
-    data () {
-      return {
-        title: 'hello big world'
-      }
-    },
-    computed: {},
-    methods: {},
-    watch: {},
-    mounted () {
-      console.log('Hello i am here!')
+  <div class="v-cart-item">
+    <vCatalog />
+    <vCart />
+  </div>
+</template>
+
+<script>
+import vCatalog from './v-catalog.vue'
+import vCart from './v-cart.vue'
+
+export default {
+  name: 'v-cart-item',
+  components: {
+    vCatalog,
+    vCart
+  },
+  props: {},
+  data () {
+    return {
+      title: 'hello big world'
     }
+  },
+  computed: {},
+  methods: {},
+  watch: {},
+  mounted () {
+    console.log('Hello i am here!')
   }
-  </script>
-  
-  <style lang="scss">
-  .v-cart-item {
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    align-items: center;
-    flex-basis: 25%;
-    box-shadow: 0 0 8px 0 greenyellow;
-    padding: 20px;
-    margin: 20px;
-  }
-  </style>
-  
+}
+</script>
+
+<style lang="scss">
+.v-cart-item {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  align-items: center;
+  flex-basis: 25%;
+  box-shadow: 0 0 8px 0 greenyellow;
+  padding: 20px;
+  margin: 20px;
+}
+</style>
